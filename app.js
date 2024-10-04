03f841ae669f208fa2e4e3b3f2e99ac668d58795
OrianAziz <orian8331@gmail.com>
2024-10-04 16:15:10 +0300

    client side

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose')
 const app = express();
 
 // נתיב ברירת מחדל - דף הבית
@@ -6,8 +7,10 @@ app.get('/', (req, res) => {
   res.send('Welcome to My Pet Shop!');
 });
 
+mongoose.connect('mongodb://localhost:27017')
+
 // השרת מאזין בפורט 3000
-const port = 3000;
+const port = 8080;
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
