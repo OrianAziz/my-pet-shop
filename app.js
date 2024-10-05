754771e0246c001b674478db337047667c1afbbe
OrianAziz <orian8331@gmail.com>
2024-10-06 01:10:31 +0300

    controller

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 const express = require('express');
 //const mongoose = require('mongoose');
-const app = express();
-const path = require('path');
-
 //mongoose.connect('mongodb://localhost:27017')
+const path = require('path');
+const app = express();
 
-const homeController = require('./controllers/homeController');
-// הגדרת תיקייה סטטית (ל-CSS, JS, תמונות וכו')
 app.use(express.static(path.join(__dirname, 'public')));
 
-// ניתוב לעמוד הבית
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views')); 
+
+const homeController = require('./controllers/homeController');
 app.get('/', homeController.getHomePage);
-const port = 3002;
+
+
+const port = 3003;
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,6 +1,5 @@
-const path = require('path');
 
-// פונקציה להצגת עמוד הבית
+
 exports.getHomePage = (req, res) => {
-    res.sendFile(path.join(__dirname, '../public', 'petshop.html'));
+    res.render('petshop'); 
 };
