bf92c15da205ea7c603071baa88092daa6db377f
OrianAziz <orian8331@gmail.com>
2024-10-05 17:40:31 +0300

    עמוד הבית css

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ const express = require('express');
 const mongoose = require('mongoose')
 const app = express();
 
-// נתיב ברירת מחדל - דף הבית
+
 app.get('/', (req, res) => {
   res.send('Welcome to My Pet Shop!');
 });
 
 mongoose.connect('mongodb://localhost:27017')
 
-// השרת מאזין בפורט 3000
+
 const port = 3001;
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
